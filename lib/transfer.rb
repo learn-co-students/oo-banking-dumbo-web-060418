f6753a9c8b889b05ca33da59f9af23b4a83c4487
michermd <michermd@gmail.com>
2018-06-07 16:09:20 -0400

    Pair with Katrina

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,42 @@
+require 'pry'
+
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :amount
+  @@all = []
+  def initialize(sender, receiver, amount=0)
+    @sender = sender
+    @receiver = receiver
+    @status = 'pending'
+    @amount = amount
+
+  end
+
+  def valid?
+    sender.valid? and receiver.valid?
+  end
+
+  def execute_transaction
+    if valid? and @status != 'complete' and self.sender.balance > amount
+      receiver.balance += @amount
+      sender.balance -= @amount
+      @status = 'complete'
+    else
+      @status = 'rejected'
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+      if @status == 'complete'
+        receiver.balance -= @amount
+        sender.balance += @amount
+        @status = 'reversed'
+      end
+  end
+
 end
+
+
+# Build BancAccount class
+# Instance of BancAccount use Transfer class to transfer money
+# Transfer instance have a status (pending, complete or rejected)
